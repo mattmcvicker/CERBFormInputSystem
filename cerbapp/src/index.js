153c17f6762de0820561a6ea859a2d546ac443f6
mattmcv <23mcvicker@gmail.com>
2021-03-13 10:52:55 -0800

    Add login frontend

--- cerbapp/src/index.js
+++ cerbapp/src/index.js
@@ -6,6 +6,8 @@ import { Router, Route, Switch, Redirect } from "react-router-dom";
 
 // core components
 import Admin from "./Admin";
+import Login from "./views/Login";
+import SignUp from "./views/SignUp";
 
 import "./css/material-dashboard-react.css";
 const hist = createBrowserHistory();
@@ -13,8 +15,11 @@ const hist = createBrowserHistory();
 ReactDOM.render(
   <Router history={hist}>
     <Switch>
+      <Route exact path='/login' component={Login} />
+      <Route path="/sign-in" component={Login} />
+      <Route path="/sign-up" component={SignUp} />
       <Route path="/admin" component={Admin} />
-      <Redirect from="/" to="/admin/dashboard" />
+      <Redirect from="/" to="/login" />
     </Switch>
   </Router>,
   document.getElementById("root")
